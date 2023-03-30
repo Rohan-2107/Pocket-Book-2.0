3e1f760347a1aaf675e90af3680d48c5b37f3788
Rohan-2107 <rohanrkc289@gmail.com>
2023-03-30 18:24:05 +0530

    created home activity layout

--- app/src/main/java/com/example/pocketbook21/HomeActivity.java
+++ app/src/main/java/com/example/pocketbook21/HomeActivity.java
@@ -1,8 +1,19 @@
 package com.example.pocketbook21;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class HomeActivity extends AppCompatActivity {
 
